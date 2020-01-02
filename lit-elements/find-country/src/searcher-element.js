ba3246979ac91ed7f9376598239d40d229e36294
Tovtin <intn@MXPC010033.local>
2020-01-02 09:38:36 -0600

    added chnages for searching

--- lit-elements/find-country/src/searcher-element.js
+++ lit-elements/find-country/src/searcher-element.js
@@ -36,33 +36,8 @@ export class SearcherElement extends LitElement {
     `;
   }
 
-  async showCountriesData(event) {
-    event.preventDefault();
-    const list = this.shadowRoot.querySelector(".country-description-js");
-    const input = this.shadowRoot.querySelector("input");
-    const inputValue = input.value.toLowerCase();
-
-    const countryNamesData = await fetchAndLogCountries().then(data =>
-      data.map(country => country.name)
-    );
-    console.log("countryNamesData:", countryNamesData);
-
-    const lettersOfName = countryNamesData.map(i => i.split(""));
-    console.log("lettersOfName:", lettersOfName);
-
-    /*  
-   
-   // METHOD OF MATCHING INPUT.VALUE WITH COUNTRY.NAME ONE BY ONE
-   
-   if (inputValue === lettersOfName) {
-    } 
-    
-    */
-
-    const markupCountry = await fetchAndLogCountries().then(data => {
-      return data
-        .map(country => {
-          return `<p class="country-name">${country.name}</p>
+  markupCountry(country) {
+    return `<p class="country-name">${country.name}</p>
           <div class="description">
             <div>
              <p><span class="headline">Capital:</span> ${country.capital}</p>
@@ -78,11 +53,55 @@ export class SearcherElement extends LitElement {
               country.flag
             }" alt="This is the flag!" class="flag" width="300px">
           </div>`;
-        })
-        .join(" ");
-    });
+  }
 
-    list.insertAdjacentHTML("beforeend", markupCountry);
+  markupCountriesNames(country) {
+    return `${country
+      .map(country => `<div class="country-name">${country.name}</div>`)
+      .join(" ")}`;
+  }
+
+  removeListItems() {
+    const alertList = this.shadowRoot.querySelector(".alert-list");
+    alertList.innerHTML = "";
+    const list = this.shadowRoot.querySelector(".country-description-js");
+    list.innerHTML = "";
+  }
+
+  async showCountriesData(event) {
+    event.preventDefault();
+    const input = this.shadowRoot.querySelector("input");
+    const inputValue = input.value.toLowerCase();
+
+    await fetchAndLogCountries(inputValue)
+      .then(countries =>
+        countries.filter(country =>
+          country.name.toLowerCase().includes(inputValue)
+        )
+      )
+      .then(result => {
+        const resultArr = Array.from(result);
+        console.log("resultArr:", resultArr);
+        if (resultArr.length === 0) {
+          console.log("warningMissingMatches");
+        } else if (inputValue.length === 0) {
+          this.removeListItems();
+        } else if (resultArr.length === 1) {
+          this.removeListItems();
+          const list = this.shadowRoot.querySelector(".country-description-js");
+          list.insertAdjacentHTML("beforeend", this.markupCountry(result[0]));
+        } else if (resultArr.length > 1 && resultArr.length <= 10) {
+          this.removeListItems();
+          const alertList = this.shadowRoot.querySelector(".alert-list");
+          alertList.insertAdjacentHTML(
+            "beforeend",
+            this.markupCountriesNames(result)
+          );
+        } else {
+          this.removeListItems();
+          console.log("warningTooManyMatches");
+        }
+      });
   }
 }
 customElements.define("searcher-element", SearcherElement);
